e4ec05b9ddb925c99dfe8316a657a062c3ec2a6c
Piyush Pandita <piyush100078@gmail.com>
2018-09-22 07:35:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 setup(name='pydes',
       version='0.1',
       description='Multi-objective Bayesian Optimization under Uncertainty',
-      url='http://github.com/storborg/funniest',
       author='Ilias Bilionis',
       author_email='ibilion@purdue.edu',
       license='MIT',
