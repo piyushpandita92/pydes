6c8218ae75ea65cb3526f46e58df9c016fbc47d6
Piyush Pandita <piyushpandita@Piyushs-MacBook-Pro.local>
2018-09-22 07:15:39 -0400

    f

--- tests/ex1.py
+++ tests/ex1.py
@@ -10,7 +10,8 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import pydes
 import numpy as np
-import design
+import GPy
+from pyDOE import *
 from scipy.optimize import minimize
 from scipy.optimize import check_grad
 from scipy.optimize import approx_fprime
@@ -18,6 +19,7 @@ from example_objective_functions import ObjFunc1
 import shutil
 
 if __name__ == '__main__':
+    np.random.seed(1222)
     assert len(sys.argv)==3
     sigma = sys.argv[1]
     n = int(sys.argv[2])
@@ -26,19 +28,20 @@ if __name__ == '__main__':
         shutil.rmtree(out_dir)
     os.makedirs(out_dir)
     dim = 2
+    max_it = 2
     obj_funcs = ObjFunc1(sigma=sigma, n_samp=1)
     obj_funcs_true = ObjFunc1(sigma=sigma, n_samp=100)
-    X_init = design.latin_center(n, dim, seed=123455)
+    X_init = lhs(dim, n)
     Y_init = np.array([obj_funcs(x) for x in X_init])
-    X_d_true = design.latin_center(10000, dim, seed=12345)
+    X_d_true = lhs(dim, 1000)
     Y_true = np.array([obj_funcs_true(x) for x in X_d_true]) 
     ehvi_opt_bounds = ((0, 1), ) * dim
     trans_function = lambda y:y
     p = pydes.ParetoFront(X_init, Y_init, obj_funcs, obj_funcs_true, 
         Y_true=Y_true,
         ehvi_opt_bounds=ehvi_opt_bounds,
-        X_design=1000,
-        max_it=100,
+        X_design=100,
+        max_it=max_it,
         do_posterior_samples=True,
         gp_fixed_noise=None,
         kernel_type=GPy.kern.Matern32,
